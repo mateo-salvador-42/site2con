3be7f6edbc82e0652bd4cd0546f74240707dfb30
Matéo SALVADOR <salvador.mateo205@gmail.com>
2026-05-12 23:19:34 +0200

    push

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,8 @@
-import { config } from "dotenv";
-import { resolve } from "path";
-config({ path: resolve(process.cwd(), ".env") });
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env["DATABASE_URL"],
   },
 });
